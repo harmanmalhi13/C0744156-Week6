65a0c7f881e2e19b15cdfbbcc489a012faee86ae
visualstudio <visualstudio@230-PC11>
2019-02-14 11:17:14 -0500

    //events

--- c0744156/js/main.js
+++ c0744156/js/main.js
@@ -1,1 +1,14 @@
 ﻿// week06
+ /*function addNumbers(j) {
+            var sum = 0;
+            for (i = 0; i < j; i++)
+            {
+                sum += i;
+            }
+            var i = 0;
+            while (i < j) {
+                sum += i;
+                i++;
+            }
+            return sum;
+        }*/
